2a5179eaeec4a7368c7677877b5dac3f4a09b3fa
xdf <772327379@qq.com>
2025-11-28 18:44:06 +0000

    添加常量定义，包括配置键、数据对象 ID、设备模型和实体键

--- custom_components/linptech_ble/const.py
+++ custom_components/linptech_ble/const.py
@@ -5,4 +5,22 @@ from logging import Logger, getLogger
 LOGGER: Logger = getLogger(__package__)
 
 DOMAIN = "linptech_ble"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+
+# 配置键
+CONF_BINDKEY = "bindkey"
+
+# 数据对象 ID（来自 ble_monitor issue #1367）
+OBJECT_ID_PRESSURE_STATE = 0x483C
+OBJECT_ID_PRESSURE_PRESENT_DURATION = 0x483D
+OBJECT_ID_PRESSURE_NOT_PRESENT_DURATION = 0x483E
+OBJECT_ID_BATTERY = 0x4C03
+
+# 设备模型
+MODEL_PS1BB = "PS1BB"
+
+# 实体键
+KEY_PRESSURE_STATE = "pressure_state"
+KEY_PRESSURE_PRESENT_DURATION = "pressure_present_duration"
+KEY_PRESSURE_NOT_PRESENT_DURATION = "pressure_not_present_duration"
+KEY_BATTERY = "battery"
+KEY_RSSI = "signal_strength"
